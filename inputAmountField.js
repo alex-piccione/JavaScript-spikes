8b610e5ee11dc088d6a056a7d2cd58a9fc833cbd
Alessandro Piccione <alex.75@tin.it>
2015-06-23 22:58:00 +0200

    Added placeholder attribute.

--- inputAmountField.js
+++ inputAmountField.js
@@ -2,7 +2,11 @@ app.directive("amountField", function(){
 	var directive = {
 		restrict: "E",
 		//templateUrl: "inputAmountFieldTemplate.html"
-		template: '<input type="text" class="form-input">'
+		template: '<input type="text" class="form-input" placeholder="{{placeholder}}">'
+	};
+	
+	directive.scope = {
+		placeholder: "=placeholder"		
 	};
 	
 	return directive;
