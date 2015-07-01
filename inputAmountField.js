40dc31c2fde2062bad4f03242ae009348d571666
Alessandro Piccione <alex.75@tin.it>
2015-07-01 23:00:16 +0200

    Added angular locale files. Adde decimalSeparator attribute to directive.

--- inputAmountField.js
+++ inputAmountField.js
@@ -3,11 +3,13 @@ app.directive("spikeAmountField", function(){
 		restrict: "E",
 		//templateUrl: "inputAmountFieldTemplate.html"
 		template: '<input type="text" class="form-input" placeholder="{{placeholder}}" value="{{amount}}">'
+			+ ' decimal separator: {{decimalSeparator}}'		
 	};
 	
 	directive.scope = {
 		amount: "=amount",
-		placeholder: "=placeholder"
+		placeholder: "=placeholder",
+		decimalSeparator: "="
 	};
 	
 	return directive;
