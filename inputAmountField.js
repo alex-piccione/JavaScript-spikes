c34fa91d84b147db82010e5a5fac224ff8894612
Alessandro Piccione <alex.75@tin.it>
2015-07-03 23:17:34 +0200

    (WIP) Evaluation of expression work only the first time

--- inputAmountField.js
+++ inputAmountField.js
@@ -2,7 +2,7 @@ app.directive("spikeAmountField", function(){
 	var directive = {
 		restrict: "E",
 		//templateUrl: "inputAmountFieldTemplate.html"
-		template: '<input type="text" class="form-input" placeholder="{{placeholder}}" value="{{amount}}" >'
+		template: '<input id="{{id}}" type="text" class="form-input" placeholder="{{placeholder}}" __value="{{amount}}" ng-model="amount" >'
 			+ ' decimal separator: {{decimalSeparator}}'		
 	};
 	
@@ -13,11 +13,68 @@ app.directive("spikeAmountField", function(){
 	};
 	
 	directive.link = function(scope, element, attr){
-		element.on("keyup", function(event){
-			alert(element.val());	
-			var amount = eval(element.val());
-			scope.amount = amount;
-		});		
+		scope.id = "spikeAmountField_" + Math.floor(Math.random()*1000000);
+		//console.log(scope.id);
+
+		//var field = element.find("#" + scope.id);
+		var isEvaluating = null;
+		scope.$watch( function(scope) {return scope.amount}, function(newValue, oldValue){
+			if(newValue !== oldValue)
+			{
+				console.log("newValue > " + newValue);
+				
+				isEvaluating && clearTimeout(isEvaluating); // stop evaluating
+				
+				
+				// normalize to JavaScript (en) locale
+				var normalizedText = (newValue+ "").replace(scope.decimalSeparator, ".");
+				//console.log("normalized: " + normalizedText);
+				// contains somethin different from numbers or decimal separator?
+				if (normalizedText.match(/[^\d.]/g))
+				{	
+					isEvaluating = setTimeout( function(){
+					
+						var valueResult = null;
+						try 
+						{
+							console.log("evaluate " + normalizedText);
+							valueResult = eval(normalizedText); 
+							console.log("amount = " + valueResult);
+							scope.amount = valueResult;	
+						}
+						catch(error)
+						{
+							//valueResult = 		normalizedText + " (cannot evaluate)";		
+							console.log("cannot evaluate \"" + normalizedText + "\".");
+						}
+					}, 1000*1);	
+					//scope.amount = 
+				}
+				
+			}
+		
+		})
+		/*
+		//element.on("keyup", function(event){
+		$(field).on("keyup", function(event){
+			var field = element.find("#" + scope.id);
+			
+			console.log(event.keyCode);
+			
+			//var char_ = event.keyCode;
+			if(event.keyCode == 13) // return
+			{
+				var amount = eval(field.val());
+				scope.amount = amount;				
+			}		
+			//else
+			//	scope.evaluate();
+			
+			//console.log(field.val());
+			
+			//var operators = ['+', '-', 'x', '÷'];	
+		});	
+		*/		
 	};
 	
 	return directive;
