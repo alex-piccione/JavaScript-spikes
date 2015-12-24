c9f330324df7fa6ff660569e41fcf1fa4b51c6dc
alex75ita <alessandro.piccione.75@gmail.com>
2015-12-24 20:38:59 +0100

    Add a scatterplot chart example

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,6 +1,7 @@
+tauCharts || tauCharts;
 
 // data
-var datasource = [{
+var datasource_1 = [{
   type:'us', count:20, date:'12-2013'
 },{
   type:'us', count:10, date:'01-2014'
@@ -10,13 +11,38 @@ var datasource = [{
   type:'bug', count:23, date:'05-2014'
 }];
 
-//
-var chart = new tauCharts.Chart({
-    data: datasource,
+
+var datasource_2 = [
+    {"team": "d", "cycleTime": 1, "effort": 1, "count": 1, "priority": "low"},
+    {"team": "k", "cycleTime": 4, "effort": 6, "count": 8, "priority": "medium"}
+];
+
+// charts
+var chart_1 = new tauCharts.Chart({
+    data: datasource_1,
     type: 'line',
     x: 'date',
     y: 'count',
     color: 'type' // there will be two lines with different colors on the chart
 });
 
-chart.renderTo('#chart_1');
+chart_1.renderTo('#chart_1');
+
+var chart_2 = new tauCharts.Chart({
+    guide: {
+        x: {label: "Cycle Time in days"},   // custom label for X axis
+        y: {label: "Effort in points"},     // custom label for Y axis
+        padding: {b:40, l:40, t:10, r:10 }, // chart paddin
+        colors: {                           // colors
+            brewer: ["color-red", "color-green", "color-blue"] 
+        }
+    },
+    data: datasource_2,
+    type: "scatterplot",
+    x: "cycleTime",
+    y: "effort",
+    color: "team", // evry team will be represented by diferent color
+    size: "count"
+});
+
+chart_2.renderTo("#chart_2");
