da1370093e89a6c58645bc0336b5d3236233028d
Alessandro Piccione <alex.75@tin.it>
2016-01-15 19:25:43 +0100

    Create method Array.prototype.any(values) that accept an array as parameter.

--- JS/Calculator.js
+++ JS/Calculator.js
@@ -1,4 +1,72 @@
-function Calculator() {
+
+// Immediately Invoked Function Expression (IIFE)
+// ref: https://en.wikipedia.org/wiki/Immediately-invoked_function_expression
+
+function Calculator(){
+    
+    this.operators = ["+", "-", "*", "/"];
+    
+    /*return {
+         
+    };*/
+};
+
+
+Calculator.prototype.sum = function(n1, n2){
+    
+    return n1+n2;
+};
+
+
+Array.prototype.any = function(values){    
+    
+    if(values === undefined)
+        return false;
+    
+    if(values === undefined || !values.hasOwnProperty("length"))
+        values = [values];  
+    
+    for(var i in values)
+    {
+        if(this.indexOf(values[i]) >= 0)
+            return true;        
+    }
+    /*
+    var _a = this;
+    values.forEach(function(e){
+        console.log(_a.indexOf(e));
+        
+        //if _a.some(function(_e,i,array){return _e == e})
+        
+        if (_a.indexOf(e) >= 0) {
+            console.log("true");
+            return true;        
+        }
+    });*/
+    
+    console.log("false");
+    return false;
+    /*
+    var _array = this;
+    values.forEach(function(eToFind){
+        _array.forEach(function(e){
+            if(e === eToFind)
+                return true;            
+        });        
+    });
+    return false;
+    */
+}
+
+Calculator.prototype.recognizeValues = function(text){
+    var startValues = text.toString().split(" ");
+    var values = [];    
+   
+    
+    return values;
+};
+
+function _Calculator() {
 
 	return {
 	
@@ -25,19 +93,29 @@ function Calculator() {
         recognizeValues: function(text){
             var values = text.toString().split(" ");
             
-            return values;
+            values.forEach(function(element, index) {
+                var sumOperator = element.indexOf("+"); 
+                if(sumOperator)
+                {
+                    //values = values.splice(0, sumOperator).
+                    
+                }    
+            });
             
-            //while (values.length > 1)
+            
+            
+            if(values.length > 1)
             {
                 if(values.length == 2)
                      throw Error("Remain 2 values to elaborate, cannot elaborate");
                 
                 var a = values[0];
-                var b = values [2];
+                var b = values[2];
                 
             } 
             
+            return values;            
         }
         
 	}
-}
+};
--- JS/calculator.jasmine.js
+++ JS/calculator.jasmine.js
@@ -5,7 +5,7 @@ describe("Calculator", function(){
     var calculator;
 
     beforeEach(function(){
-        calculator  = Calculator();    
+        calculator = new Calculator();    
     });
 
     describe("sum", function() {
@@ -38,6 +38,21 @@ describe("Calculator", function(){
             var expectedResult = ["1", "+", "2"];
             var result = calculator.recognizeValues(input);
             expect(result).toEqual(expectedResult);
+        });
+        
+    });
+    
+    describe("Array.any()", function(){
+        
+        it("should exists", function(){
+            var a = [];
+            expect(a.any).toBeDefined();            
+        });
+        
+        it("given the array \"[1,2,5,6]\" should return true for \"2\"", function(){
+            var array = [1,2,5,6];
+            //expect(array.any(2)).toBeTruthy();            
+            expect(array.any(2)).toBe(true);
         });
         
     });
